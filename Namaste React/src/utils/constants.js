e920f14d94bece936dd263b7bdedb1d80e10af11
Payal Gupta <payalgupta020405@gmail.com>
2026-06-03 15:24:32 +0530

    Delete Namaste React directory

--- Namaste React/src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const LOGO_URL = "https://cdn-icons-png.flaticon.com/512/3075/3075977.png";
-
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
